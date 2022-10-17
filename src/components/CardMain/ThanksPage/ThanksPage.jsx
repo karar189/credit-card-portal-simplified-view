edf7ddf0efd4ce6bd6d69bf4327f03b56455dd99
swetakarar <kararsweta@gmail.com>
2022-10-17 20:58:04 +0530

    style: thank you card

--- src/components/CardMain/ThanksPage/ThanksPage.jsx
+++ src/components/CardMain/ThanksPage/ThanksPage.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-import "./style.css";
+import "../ThanksPage/style.css";
+import tick from "../../../images/icon-complete.svg";
+import { Container, Box, Grid } from "@mui/material";
 
 const ThanksPage = () => {
-  return <div>ThanksPage</div>;
+  return (
+    <>
+      <div className="thanks-box">
+        <img src={tick} alt="" />
+        <h1>THANK YOU!</h1>
+        <p>We've added your card details.</p>
+        <br />
+        <div className="form-button ">
+          <button
+            id="thanks-button"
+            style={{ border: "none", margin: "0" }}
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            {/* <ColorButton variant="contained">SUBMIT</ColorButton> */}
+            <p>Continue</p>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ThanksPage;
